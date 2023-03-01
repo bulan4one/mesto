3a4ccc45f57bba83b7380ac88c94a3a1e4905c1b
Alex Bulan <bulan4one@yandex.ru>
2023-03-01 22:25:54 +0700

    fix

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,31 @@
+const initialCards = [
+  {
+    name: "Архыз",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+    alt: "Архыз"
+  },
+  {
+    name: "Челябинская область",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
+  },
+  {
+    name: "Иваново",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
+  },
+  {
+    name: "Камчатка",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
+  },
+  {
+    name: "Холмогорский район",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
+  },
+  {
+    name: "Байкал",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+  },
+];
+
 let profileName = document.querySelector(".profile__name");
 let profileDescription = document.querySelector(".profile__description");
 let nameInput = document.querySelector(".popup__input_type_username");
@@ -25,3 +53,31 @@ formElement.addEventListener("submit", function (event) {
   profileDescription.textContent = jobInput.value;
   popupClose();
 });
+
+const elements = document.querySelector(".elements");
+
+// //Создание карточек по-старинке
+
+// initialCards.forEach(function (card) {
+//   const cardsHTML = `<article class="element">
+//   <button type="button" class="element__delete">Удалить</button>
+//   <img src="${card.link}" alt="${card.alt}" class="element__img">
+//   <div class="element__block">
+//     <h2 class="element__title">${card.name}</h2>
+//     <button type="button" title="Лайк" class="element__likes"></button>
+//   </div>
+// </article>`;
+
+//   elements.insertAdjacentHTML("beforeend", cardsHTML);
+// });
+
+//Создание карточек по шаблону
+
+initialCards.forEach(function(cards) {
+  const cardTemplate = document.querySelector('#cardTemplate').content.cloneNode(true)
+  elements.append(cardTemplate)
+  const cardHeading = cardTemplate.querySelector('.element__title')
+  cardHeading.textContent = 'bladsfdsafa'
+  const cardImage = cardTemplate.querySelector('.element__img')
+  cardImage.setAttribute('src', initialCards.link)
+})
