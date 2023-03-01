b739918d7bec63d967e98ef3c17e046d1c283241
Alex Bulan <bulan4one@yandex.ru>
2023-03-01 23:36:20 +0700

    popup

--- scripts/index.js
+++ scripts/index.js
@@ -2,27 +2,32 @@ const initialCards = [
   {
     name: "Архыз",
     link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
-    alt: "Архыз"
+    alt: "Архыз",
   },
   {
     name: "Челябинская область",
     link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
+    alt: "Архыз",
   },
   {
     name: "Иваново",
     link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
+    alt: "Архыз",
   },
   {
     name: "Камчатка",
     link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
+    alt: "Архыз",
   },
   {
     name: "Холмогорский район",
     link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
+    alt: "Архыз",
   },
   {
     name: "Байкал",
     link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+    alt: "Архыз",
   },
 ];
 
@@ -30,54 +35,49 @@ let profileName = document.querySelector(".profile__name");
 let profileDescription = document.querySelector(".profile__description");
 let nameInput = document.querySelector(".popup__input_type_username");
 let jobInput = document.querySelector(".popup__input_type_description");
-let popup = document.querySelector(".popup");
+let popupChangeProfile = document.querySelector("#popup-change-profile");
 let changeNameButton = document.querySelector("#change-profile-name");
 
 changeNameButton.addEventListener("click", function (event) {
   nameInput.value = profileName.textContent;
   jobInput.value = profileDescription.textContent;
-  popup.classList.add("popup_opened");
+  popupChangeProfile.classList.add("popup_opened");
 });
 
-function popupClose() {
+function popupClose(popup) {
   popup.classList.remove("popup_opened");
 }
 
 let closePopup = document.querySelector(".popup__close");
-closePopup.addEventListener("click", popupClose);
+closePopup.addEventListener("click", popupClose.apply(null, popupChangeProfile));
 
 let formElement = document.querySelector("#form-edit");
 formElement.addEventListener("submit", function (event) {
   event.preventDefault();
   profileName.textContent = nameInput.value;
   profileDescription.textContent = jobInput.value;
-  popupClose();
+  popupClose(popupChangeProfile);
 });
 
+//Создание карточек по шаблону
 const elements = document.querySelector(".elements");
 
-// //Создание карточек по-старинке
+initialCards.forEach(function (card) {
+  const cardTemplate = document
+    .querySelector("#cardTemplate")
+    .content.cloneNode(true);
+  const cardHeading = cardTemplate.querySelector(".element__title");
+  cardHeading.textContent = card.name;
+  const cardImage = cardTemplate.querySelector(".element__img");
+  cardImage.setAttribute("src", card.link);
+  cardImage.setAttribute("alt", card.alt);
+  elements.append(cardTemplate);
+});
 
-// initialCards.forEach(function (card) {
-//   const cardsHTML = `<article class="element">
-//   <button type="button" class="element__delete">Удалить</button>
-//   <img src="${card.link}" alt="${card.alt}" class="element__img">
-//   <div class="element__block">
-//     <h2 class="element__title">${card.name}</h2>
-//     <button type="button" title="Лайк" class="element__likes"></button>
-//   </div>
-// </article>`;
+let popupAddCard = document.querySelector("#popup-add-card");
+let buttonOpen = document.querySelector(".profile__add-place");
+buttonOpen.addEventListener("click", function (event) {
+  popupAddCard.classList.add("popup_opened");
 
-//   elements.insertAdjacentHTML("beforeend", cardsHTML);
-// });
-
-//Создание карточек по шаблону
-
-initialCards.forEach(function(cards) {
-  const cardTemplate = document.querySelector('#cardTemplate').content.cloneNode(true)
-  elements.append(cardTemplate)
-  const cardHeading = cardTemplate.querySelector('.element__title')
-  cardHeading.textContent = 'bladsfdsafa'
-  const cardImage = cardTemplate.querySelector('.element__img')
-  cardImage.setAttribute('src', initialCards.link)
-})
+  event.preventDefault();
+});
